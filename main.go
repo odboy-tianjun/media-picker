953c3ad067577d947f794a71aac8cd812358307d
Odboy <tianjun@odboy.cn>
2024-07-16 19:11:35 +0800

    upd:改版

--- main.go
+++ main.go
@@ -8,16 +8,16 @@ import (
 	_ "image/gif"  // 导入gif支持
 	_ "image/jpeg" // 导入jpeg支持
 	_ "image/png"  // 导入png支持
+	"os"
 	"time"
 )
 
 func main() {
-	//rootDir, err := os.Getwd()
-	//if err != nil {
-	//	fmt.Println("=== 获取当前路径异常", err)
-	//	return
-	//}
-	rootDir := "G:\\DZL_DATA\\D20240630\\Video\\other"
+	rootDir, err := os.Getwd()
+	if err != nil {
+		fmt.Println("=== 获取当前路径异常", err)
+		return
+	}
 	scanner := core.FileScanner{}
 	scanner.DoScan(rootDir)
 	scanner.DoFilter()
